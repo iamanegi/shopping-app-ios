493918239f3f7ef394548d3912afb0539120af5d
aman <amannegi229@gmail.com>
2022-09-10 13:46:46 +0530

    Initial Commit

---
+++ shopping/shoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingApp.swift
+//  shopping
+//
+//  Created by Aman Negi on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct shoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
